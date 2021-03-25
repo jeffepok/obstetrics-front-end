da03e5499cad89e827f16aedec7ef1ee922d9f97
jefferson <jeffepok@gmail.com>
2021-03-25 21:01:28 +0000

    Added data inputs for obstetric history in tables view

--- src/app/components/cards/card-inputs/card-inputs.component.ts
+++ src/app/components/cards/card-inputs/card-inputs.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-card-inputs',
@@ -6,8 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-inputs.component.css']
 })
 export class CardInputsComponent implements OnInit {
+  @Input()
+  get color(): string {
+    return this._color;
+  }
+  set color(color: string) {
+    this._color = color !== 'light' && color !== 'dark' ? 'light' : color;
+  }
 
+  private _color = 'light';
   constructor() { }
+
+  public toInitialUpperCase(word: string): string{
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }
 
   ngOnInit(): void {
   }
